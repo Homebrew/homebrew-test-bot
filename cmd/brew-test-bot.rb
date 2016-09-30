cc3439f0bb8dd993f6c659760e5aeb928bd5dea8
Mike McQuaid <mike@mikemcquaid.com>
2016-09-30 08:32:58 +0100

    Only run `brew style $FORMULA` on homebrew/core. (#5)
    Other taps haven't had the change to have a mass-update yet and we'll
    hold off doing that until we get our own house in order.

--- cmd/brew-test-bot.rb
+++ cmd/brew-test-bot.rb
@@ -633,7 +633,7 @@ module Homebrew
           install_passed = steps.last.passed?
         end
       end
-      test "brew", "style", formula_name
+      test "brew", "style", formula_name if @tap && @tap.name == "homebrew/core"
       test "brew", "audit", *audit_args
       if install_passed
         if formula.stable? && !ARGV.include?("--fast") && !ARGV.include?("--no-bottle") && !formula.bottle_disabled?
