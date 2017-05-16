09f23b03b6da7e944192ad0e7c9b7911fe730228
Mike McQuaid <mike@mikemcquaid.com>
2017-05-16 13:31:14 +0100

    Test `master` build and try to debug it.
    This is failing on Homebrew/brew, not sure if failing here.

--- cmd/brew-test-bot.rb
+++ cmd/brew-test-bot.rb
@@ -924,7 +924,7 @@ module Homebrew
 
       if ENV["TRAVIS"]
         # For Travis CI build caching.
-        test "brew", "install", "md5deep" if OS.mac?
+        test "brew", "install", "md5deep", "--debug" if OS.mac?
         return
       end
 
