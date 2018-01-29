f796845c2e246e06998622c089c844679ed1a868
Mike McQuaid <mike@mikemcquaid.com>
2018-01-29 09:53:38 -0800

    testbottest: use a few more DSL.
    Let's make this integration test a little bit fuller.

--- Formula/testbottest.rb
+++ Formula/testbottest.rb
@@ -1,11 +1,25 @@
 class Testbottest < Formula
-  desc "Minimal C program and Makefile used for testing Homebrew."
+  desc "Minimal C program and Makefile used for testing Homebrew"
   homepage "https://github.com/Homebrew/brew"
   url "file://#{File.expand_path("..", __FILE__)}/tarballs/testbottest-0.1.tbz"
   sha256 "246c4839624d0b97338ce976100d56bd9331d9416e178eb0f74ef050c1dbdaad"
+  head "https://github.com/Homebrew/homebrew-test-bot.git"
+
+  depends_on :java => ["1.0+", :optional]
+
+  fails_with :gcc do
+    build 1
+    cause "testing"
+  end
 
   def install
+    odie "whoops, shouldn't be using java!" if build.with?(:java)
+
     system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  def post_install
+    system "#{bin}/testbottest"
   end
 
   test do
