fef71151514336369180af628ede4fecc01710e1
Mike McQuaid <mike@mikemcquaid.com>
2018-03-06 10:01:12 +0000

    testbottest: use simpler expand_path.

--- Formula/testbottest.rb
+++ Formula/testbottest.rb
@@ -1,7 +1,7 @@
 class Testbottest < Formula
   desc "Minimal C program and Makefile used for testing Homebrew"
   homepage "https://github.com/Homebrew/brew"
-  url "file://#{File.expand_path("..", __FILE__)}/tarballs/testbottest-0.1.tbz"
+  url "file://#{File.expand_path(__dir__)}/tarballs/testbottest-0.1.tbz"
   sha256 "246c4839624d0b97338ce976100d56bd9331d9416e178eb0f74ef050c1dbdaad"
   head "https://github.com/Homebrew/homebrew-test-bot.git"
 
