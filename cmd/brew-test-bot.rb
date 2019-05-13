9fcd83d318265dc88a8cf6b8751e18e91d43be26
Ladislas de Toldi <ladislas@leka.io>
2019-05-13 22:11:49 +0200

    add ohai to help debug start and end diff sha1

--- cmd/brew-test-bot.rb
+++ cmd/brew-test-bot.rb
@@ -533,12 +533,17 @@ module Homebrew
           "git", "-C", @tap.path.to_s,
                  "log", "-1", "--format=%h (%s)", "origin/master"
         ).strip
-        puts "#{@tap} origin/master #{tap_origin_master_revision}"
         tap_revision = Utils.popen_read(
           "git", "-C", @tap.path.to_s,
                  "log", "-1", "--format=%h (%s)"
         ).strip
-        puts "#{@tap} HEAD #{tap_revision}"
+        puts <<~EOS
+          Testing tap #{@tap} with:
+            - origin/master   - #{tap_origin_master_revision.blank? ? "(undefined)" : tap_origin_master_revision}
+            - HEAD            - #{tap_revision.blank? ? "(undefined)" : tap_revision}
+            - diff_start_sha1 - #{diff_start_sha1.blank? ? "(undefined)" : diff_start_sha1}
+            - diff_end_sha1   - #{diff_end_sha1.blank? ? "(undefined)" : diff_end_sha1}
+        EOS
       end
 
       return if diff_start_sha1 == diff_end_sha1
