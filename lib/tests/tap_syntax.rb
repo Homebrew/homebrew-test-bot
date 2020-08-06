46be4884c2780839c083534419ff22dbe3500da7
Mike McQuaid <mike@mikemcquaid.com>
2020-08-06 09:43:38 +0100

    tap_syntax: audit a single tap.
    Requires https://github.com/Homebrew/brew/pull/8231.

--- lib/tests/tap_syntax.rb
+++ lib/tests/tap_syntax.rb
@@ -12,8 +12,7 @@ module Homebrew
                                 MacOS.active_developer_dir == "/Applications/Xcode.app/Contents/Developer"
         test "brew", "style", tap.name unless broken_xcode_rubygems
 
-        has_formula_files = tap.formula_dir.entries.any? { |p| p.extname == ".rb" }
-        test "brew", "audit", "--skip-style" if has_formula_files
+        test "brew", "audit", "--tap=#{tap.name}"
       end
     end
   end
