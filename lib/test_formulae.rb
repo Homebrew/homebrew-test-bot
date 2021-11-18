e41d68eb6ad35b01595597ed7fee791a33453449
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-18 13:07:31 +0800

    test_formulae: check deps of `:all` bottles in `#bottled?`
    We currently have a few places elsewhere that check for `:all` bottle
    dependencies separately. We can simplify those by building the `:all`
    bottle logic into the `#bottled?` method.

--- lib/test_formulae.rb
+++ lib/test_formulae.rb
@@ -13,8 +13,12 @@ module Homebrew
 
       protected
 
-      def bottled?(formula, tag = nil, no_older_versions: false)
-        formula.bottle_specification.tag?(Utils::Bottles.tag(tag), no_older_versions: no_older_versions)
+      def bottled?(formula, no_older_versions: false)
+        if formula.bottle_specification.tag?(Utils::Bottles.tag(:all))
+          formula.deps.all? { |dep| bottled?(dep.to_formula, no_older_versions: no_older_versions) }
+        else
+          formula.bottle_specification.tag?(Utils::Bottles.tag, no_older_versions: no_older_versions)
+        end
       end
 
       def skipped(formula_name, reason)
