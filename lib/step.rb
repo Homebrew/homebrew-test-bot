c4c6e3d676295994c30268b8d90e1a9a3d5ff081
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-19 16:44:58 +0800

    Show annotations even with `@verbose` + refactoring
    We probably still want annotations even if `--verbose` is passed to
    `test-bot`. Also, make sure that `--verbose` respects `--fail-fast`.
    
    Finally, let's do some more refactoring since the `unless output.empty?`
    block was getting quite complicated. This results in a little
    duplication, but it's probably worth it.

--- lib/step.rb
+++ lib/step.rb
@@ -110,9 +110,30 @@ module Homebrew
     end
 
     def puts_full_output
+      return if @output.blank? || @verbose
+
       puts_in_github_actions_group("Full #{command_short} output") do
         puts @output
       end
+    end
+
+    def annotation_location(name)
+      formula = Formulary.factory(name)
+      method_sym = command.second.to_sym
+      method_location = formula.method(method_sym).source_location if formula.respond_to?(method_sym)
+
+      if method_location.present? && (method_location.first == formula.path.to_s)
+        method_location
+      else
+        [formula.path, nil]
+      end
+    rescue FormulaUnavailableError
+      [@repository.glob("**/#{name}*").first, nil]
+    end
+
+    def truncate_output(output, kb:, lines:)
+      max_length_start = [@output.length - (kb * 1024), 0].max
+      annotation_output = @output[max_length_start..].lines.last(lines).join
     end
 
     def run(dry_run: false, fail_fast: false)
@@ -149,71 +170,55 @@ module Homebrew
       output = result.merged_output
 
       # ActiveSupport can barf on some Unicode so don't use .present?
-      unless output.empty?
-        output.force_encoding(Encoding::UTF_8)
-
-        @output = if output.valid_encoding?
-          output
-        else
-          output.encode!(Encoding::UTF_16, invalid: :replace)
-          output.encode!(Encoding::UTF_8)
-        end
-
-        if @verbose
-          puts
-          return
-        end
-
-        return if passed?
-
-        puts_full_output
-
-        unless in_github_actions?
-          puts
-          exit 1 if fail_fast && failed?
-          return
-        end
-
-        os_string = if OS.mac?
-          str = +"macOS #{MacOS.version.pretty_name} (#{MacOS.version})"
-          str << " on Apple Silicon" if Hardware::CPU.arm?
-        else
-          OS.kernel_name
-        end
-
-        @named_args.each do |name|
-          next if name.blank?
-
-          path, line = begin
-            formula = Formulary.factory(name)
-            method_sym = command.second.to_sym
-            method_location = formula.method(method_sym).source_location if formula.respond_to?(method_sym)
-
-            if method_location.present? && (method_location.first == formula.path.to_s)
-              method_location
-            else
-              [formula.path, nil]
-            end
-          rescue FormulaUnavailableError
-            [@repository.glob("**/#{name}*").first, nil]
-          end
-          next if path.blank?
-
-          # GitHub Actions has a 64KB maximum for annotiations. That's a bit
-          # too long so instead let's go for a maximum of 24KB or 256 lines.
-          max_length_start = [@output.length - (24 * 1024), 0].max
-          annotation_output = @output[max_length_start..].lines.last(256).join
-
-          annotation_title = "`#{command_trimmed}` failed on #{os_string}!"
-          file = path.to_s.delete_prefix("#{@repository}/")
-          puts_in_github_actions_group("Truncated #{command_short} output") do
-            puts_github_actions_annotation(annotation_output, annotation_title, file, line)
-          end
-        end
-
+      if output.empty?
         puts
+        exit 1 if fail_fast && failed?
+        return
       end
 
+      output.force_encoding(Encoding::UTF_8)
+      @output = if output.valid_encoding?
+        output
+      else
+        output.encode!(Encoding::UTF_16, invalid: :replace)
+        output.encode!(Encoding::UTF_8)
+      end
+
+      return if passed?
+
+      puts_full_output
+
+      unless in_github_actions?
+        puts
+        exit 1 if fail_fast && failed?
+        return
+      end
+
+      os_string = if OS.mac?
+        str = +"macOS #{MacOS.version.pretty_name} (#{MacOS.version})"
+        str << " on Apple Silicon" if Hardware::CPU.arm?
+      else
+        OS.kernel_name
+      end
+
+      @named_args.each do |name|
+        next if name.blank?
+
+        path, line = annotation_location(name)
+        next if path.blank?
+
+        # GitHub Actions has a 64KB maximum for annotiations. That's a bit
+        # too long so instead let's go for a maximum of 24KB or 256 lines.
+        annotation_output = truncate_output(@output, kb: 24, lines: 256)
+
+        annotation_title = "`#{command_trimmed}` failed on #{os_string}!"
+        file = path.to_s.delete_prefix("#{@repository}/")
+        puts_in_github_actions_group("Truncated #{command_short} output") do
+          puts_github_actions_annotation(annotation_output, annotation_title, file, line)
+        end
+      end
+
+      puts
       exit 1 if fail_fast && failed?
     end
   end
