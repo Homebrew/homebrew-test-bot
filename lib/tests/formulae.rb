1f7e6965b13a825ed40544292bbe0abde66239de
Nanda H Krishna <me@nandahkrishna.com>
2023-03-02 11:36:33 -0500

    lib/tests/formulae: pass `--formula` to `brew livecheck`

--- lib/tests/formulae.rb
+++ lib/tests/formulae.rb
@@ -225,7 +225,7 @@ module Homebrew
         return unless formula.livecheckable?
         return if formula.livecheck.skip?
 
-        livecheck_step = test "brew", "livecheck", "--json", "--full-name", formula.full_name
+        livecheck_step = test "brew", "livecheck", "--formula", "--json", "--full-name", formula.full_name
 
         return if livecheck_step.failed?
         return unless livecheck_step.output?
