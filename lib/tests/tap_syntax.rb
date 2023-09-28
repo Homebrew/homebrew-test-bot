744998b8a0422c2d33ad23f8dbebd8a008bc7034
Bo Anderson <mail@boanderson.me>
2023-09-28 15:44:33 +0100

    tests/tap_syntax: simplify and speed up

--- lib/tests/tap_syntax.rb
+++ lib/tests/tap_syntax.rb
@@ -9,32 +9,10 @@ module Homebrew
 
         test "brew", "style", tap.name
 
-        if tap.core_tap? || tap.core_cask_tap?
-          if %w[push merge_group].include?(ENV["GITHUB_EVENT_NAME"])
-            test "brew", "readall", "--aliases", "--os=all", "--arch=all", tap.name
-            test "brew", "audit", "--tap=#{tap.name}"
-          end
+        return if tap.formula_files.blank? && tap.cask_files.blank?
 
-          test_api_generation
-        elsif tap.formula_files.present? || tap.cask_files.present?
-          test "brew", "readall", "--aliases", "--os=all", "--arch=all", tap.name
-          test "brew", "audit", "--tap=#{tap.name}"
-        end
-      end
-
-      private
-
-      def test_api_generation
-        FileUtils.mkdir_p "api"
-        Pathname("api").cd do
-          if tap.core_tap?
-            test "brew", "generate-formula-api"
-          else
-            test "brew", "generate-cask-api"
-          end
-        end
-      ensure
-        FileUtils.rm_rf "api"
+        test "brew", "readall", "--aliases", "--os=all", "--arch=all", tap.name
+        test "brew", "audit", "--except=installed", "--tap=#{tap.name}"
       end
     end
   end
