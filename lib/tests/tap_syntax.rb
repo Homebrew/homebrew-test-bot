b4ebdbdc77b31f43e6d9d889c539f8436b84894d
Issy Long <me@issylong.com>
2024-08-19 18:11:01 +0100

    Run `brew typecheck` as part of (some) `tap_syntax` tests

--- lib/tests/tap_syntax.rb
+++ lib/tests/tap_syntax.rb
@@ -3,9 +3,17 @@
 module Homebrew
   module Tests
     class TapSyntax < Test
+      # Eventually these should be all `tap.official?` taps except core & cask?
+      SORBET_TAPS = %w[
+        homebrew/command-not-found
+        homebrew/services
+      ].freeze
+
       def run!(args:)
         test_header(:TapSyntax)
         return unless tap.installed?
+
+        test "brew", "typecheck", tap.name if SORBET_TAPS.include?(tap.name)
 
         test "brew", "style", tap.name unless args.stable?
 
