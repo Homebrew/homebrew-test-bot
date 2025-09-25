cd33e75403dcba7c707e8f4697b70069996f1eca
Mike McQuaid <mike@mikemcquaid.com>
2025-09-25 08:50:03 +0100

    Deprecate tap and migrate to Homebrew/brew.

--- spec/stub/utils.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-def quiet_system(*) end
-
-module Formatter
-  module_function
-
-  def headline(*) end
-end
--- spec/stub/utils/analytics.rb
+++
@@ -1,9 +0,0 @@
-# typed: true
-# frozen_string_literal: true
-
-module Utils
-  module Analytics
-    sig { params(command: String, passed: T::Boolean).void }
-    def self.report_test_bot_test(command, passed); end
-  end
-end
